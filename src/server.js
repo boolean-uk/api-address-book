483a8c27761c6e74a64e902d44bd48aa7e92a238
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-06-17 16:45:17 +0100

    Sami Saeed

--- src/server.js
+++ src/server.js
@@ -1,13 +1,52 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 // write your app code here
 
+const contacts = require("/Users/samisaeed/Boolean/api-express-address-book/data/contacts.js");
 
-module.exports = app
+app.get("/contacts", (req, res) => {
+  res.json({
+    contacts,
+  });
+});
+app.get("/contacts/:id", (req, res) => {
+  const contactId = Number(req.params.id);
+  const findContact = contacts.find((contact) => contact.id === contactId);
+  res.status(200).json({ contact: findContact });
+});
+app.post("/contacts", (req, res) => {
+  const contact = req.body;
+  contact.id = 3;
+  contacts.push(contact);
+  res.status(201).json({ contact });
+});
+app.put("/contacts/:id", (req, res) => {
+  const newContact = req.body;
+  const contactID = Number(req.params.id);
+  const foundContact = contacts.find((contact) => contact.id === contactID);
+  const foundContactIndex = contacts.indexOf(foundContact);
+
+  newContact.id = contactID;
+
+  contacts.splice(foundContactIndex, 1, newContact);
+
+  res.json({ contact: newContact });
+});
+app.delete("/contacts/:id", (req, res) => {
+  const contactId = Number(req.params.id);
+  const contactIndex = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  const deletedContact = contacts[contactIndex];
+  contacts.splice(contactIndex, 1);
+  res.json({ contact: deletedContact });
+});
+
+module.exports = app;
