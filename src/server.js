227402954bf531d57abab2665be6cdb7c046681a
Tim Zoltie <t.zoltie@gmail.com>
2024-06-17 13:06:34 +0100

    post contacts

--- src/server.js
+++ src/server.js
@@ -4,12 +4,52 @@ const cors = require("cors")
 const app = express()
 const contacts = require('../data/contacts.js')
 
+let createContact = {
+    id: 0,
+    firstName: 'string',
+    lastName: 'string',
+    street: 'string',
+    city: 'string',
+    type: 'string',
+    email: 'string',
+    linkedin: 'string',
+    twitter: 'string'
+}
+
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
 app.get('/contacts', (req, res) => {
     res.status(200).json({contacts: contacts})
+})
+
+app.post('/contacts', (req, res) => {
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const street = req.body.street
+    const city = req.body.city
+    const type = req.body.type
+    const email = req.body.email
+    const linkedin = req.body.linkedin
+    const twitter = req.body.twitter
+    const newID = contacts.reverse().find((c) => c.id)
+
+    createContact.id = newID.id += 1
+    createContact.firstName = firstName
+    createContact.lastName = lastName
+    createContact.street = street
+    createContact.city = city
+    createContact.type = type
+    createContact.email = email
+    createContact.linkedin = linkedin
+    createContact.twitter = twitter
+    
+    console.log(createContact)
+    contacts.push(createContact)
+    res.status(201).json({
+        createContact
+    })
 })
 
 
