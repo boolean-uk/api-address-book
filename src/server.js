3b1c983fa2fb5626829875fdffb3c716f7453e61
Will Baxter <bax.will@googlemail.com>
2024-06-17 12:02:24 +0100

    Server updates contacts by ID

--- src/server.js
+++ src/server.js
@@ -1,38 +1,57 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const contacts = require('../data/contacts.js')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const contacts = require("../data/contacts.js");
+const app = express();
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 // write your app code here
 
-app.get('/contacts', (req, res) => {
-    res.status(200).json({contacts})
-})
+app.get("/contacts", (req, res) => {
+  res.status(200).json({ contacts });
+});
 
-app.post('/contacts', (req, res) => {
-    const newContact = req.body
-    newContact.id = contacts.length + 1
-    contacts.push(newContact)
-    res.status(201).json(newContact)
-})
+app.post("/contacts", (req, res) => {
+  const contact = req.body;
+  contact.id = contacts.length + 1;
+  contacts.push(contact);
+  res.status(201).json({ contact });
+});
 
-app.get('/contacts/:id', (req, res) => {
-    const contactId = req.params.id
-    const contact = contacts.find((contact) => contact.id === Number(contactId))
-    res.status(200).json({contact})
-})
+app.get("/contacts/:id", (req, res) => {
+  const contactId = req.params.id;
+  const contact = contacts.find((contact) => contact.id === Number(contactId));
+  if (!contact) {
+    return res.status(404).json({ error: "No contact with that ID" });
+  }
+  res.status(200).json({ contact });
+});
 
-app.delete('/contacts/:id', (req, res) => {
-    const contactId = req.params.id
-    const contact = contacts.find((contact) => contact.id === Number(contactId))
-    const index = contacts.indexOf(contact)
-    contacts.splice(index, 1)
-    res.status(200).json({contact})
-})
+app.delete("/contacts/:id", (req, res) => {
+  const contactId = req.params.id;
+  const contact = contacts.find((contact) => contact.id === Number(contactId));
+  if (!contact) {
+    return res.status(404).json({ error: "No contact with that ID" });
+  }
+  const index = contacts.indexOf(contact);
+  contacts.splice(index, 1);
+  res.status(200).json({ contact });
+});
 
-module.exports = app
+app.put("/contacts/:id", (req, res) => {
+  const contactId = req.params.id;
+  const updatedProperties = req.body;
+  let contact = contacts.find((contact) => contact.id === Number(contactId));
+
+  if (!contact) {
+    return res.status(404).json({ error: "No contact with that ID" });
+  }
+
+  Object.assign(contact, updatedProperties);
+  res.status(200).json({ contact });
+});
+
+module.exports = app;
