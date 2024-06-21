bb25d8449bffe6decd913ab5b21ad610239ef976
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-21 15:58:07 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -10,39 +10,43 @@ app.use(express.json());
 
 // write your app code here
 app.get("/contacts", (req, res) => {
-	res.status(HTTPCodes.default.Success.OK).json(contacts);
+	res.status(HTTPCodes.default.Success.OK).json({ contacts });
 });
 
 app.post("/contacts", (req, res) => {
-	contacts.push(req.body);
-	res.status(HTTPCodes.default.Success.CREATED).json();
+	const contact = {
+		id: contacts[contacts.length - 1]?.id + 1 || 0,
+		...req.body,
+	};
+	contacts.push(contact);
+	res.status(HTTPCodes.default.Success.CREATED).json({ contact });
 });
 
 app.get("/contacts/:id", (req, res) => {
 	const contact = contacts.find((e) => e?.id == req.params.id);
-	res.status(HTTPCodes.default.Success.OK).json(contact);
+	res.status(HTTPCodes.default.Success.OK).json({ contact });
 });
 
-app.delete(
-	"/contacts/:id",
-	(req, res) =>
-		contacts.splice(
+app.delete("/contacts/:id", (req, res) =>
+	res.status(HTTPCodes.default.Success.OK).json({
+		contact: contacts.splice(
 			contacts.findIndex((e) => e?.id == req.params.id),
 			1
-		) && res.status(HTTPCodes.default.Success.OK).json()
+		)[0],
+	})
 );
 
-app.put(
-	"/contacts/:id",
-	(req, res) =>
-		contacts.splice(
-			contacts.findIndex((e) => e?.id == req.params.id),
-			1,
-			{
-				...contacts.find((e) => e?.id == req.params.id),
-				...req.body,
-			}
-		) && res.status(HTTPCodes.default.Success.OK).json()
-);
+app.put("/contacts/:id", (req, res) => {
+	const idx = contacts.findIndex((e) => e?.id == req.params.id),
+		contact = idx >= 0 && { ...contacts[idx], ...req.body };
+
+	console.log(contact);
+
+	if (!contact) return res.status(402).json("Could not find contact");
+
+	contacts.splice(idx, 1, contact);
+
+	res.status(HTTPCodes.default.Success.OK).json({ contact });
+});
 
 module.exports = app;
