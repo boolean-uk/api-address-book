ec4bfbc372f325a7e6fbe17cdc90f979ef956dd6
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-06-21 15:49:52 +0100

    core done

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ let contacts = [
     {id: 2, firstName: 'Nathan', lastName: 'King'},
     {id: 3, firstName: 'Lewis', lastName: 'Capaldi'}
 ]
+let nextId = 4
 
 app.get('/contacts', (request, respond) => {
     respond.status(200).json({ contacts })
@@ -25,9 +26,41 @@ app.get('/contacts/:id', (request, respond) => {
     if (contact) {
         respond.status(200).json({ contact })
     } else {
-        respond.status(404).send('Contact not found')
+        //respond.status(404).send('Contact not found')
+       respond.status(404).json({ contact })
     }
 }) 
+
+app.post('/contacts', (request, respond) => {
+   const newContact = request.body
+   newContact.id = nextId++
+   contacts.push(newContact)
+   respond.status(201).json({ contact: newContact })
+
+})
+
+app.put('/contacts/:id', (request, respond) => {
+    const id = parseInt(request.params.id)
+    const index = contacts.findIndex (c => c.id === id)
+    if (index !== -1) {
+        contacts[index] = {...contacts[index], ...request.body}
+        respond.status(200).json({contacts: contacts[index]})
+    } else {
+        respond.status(404).json('Contacts not found')
+    }
+   
+})
+
+app.delete('/contacts/:id', (request, respond) => {
+    const id = parseInt(request.params.id)
+    const index = contacts.findIndex(c => c.id === id)
+    if (index !== -1) {
+        const deleteContact = contacts.splice(index, 1)
+        respond.status(200).json ({ contact: deleteContact[0] })
+    } else {
+        respond.status(404).json('contacts not found')
+    }
+})
 
 
 module.exports = app
