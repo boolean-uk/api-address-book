4258362967e9cc60a01b1db9ba9c38b3699298fd
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-06-21 14:22:52 +0100

    oo

--- src/server.js
+++ src/server.js
@@ -9,9 +9,25 @@ app.use(express.json())
 
 // write your app code here
 
-app.get('/', (request, respond) => {
-    respond.send('Hello world')
+let contacts = [
+    {id: 1, firstName: 'Abiodun', lastName: 'Oluwagbemi'}, 
+    {id: 2, firstName: 'Nathan', lastName: 'King'},
+    {id: 3, firstName: 'Lewis', lastName: 'Capaldi'}
+]
+
+app.get('/contacts', (request, respond) => {
+    respond.status(200).json({ contacts })
 })
+
+app.get('/contacts/:id', (request, respond) => {
+    const id = parseInt(request.params.id)
+    const contact = contacts.find(c => c.id === id)
+    if (contact) {
+        respond.status(200).json({ contact })
+    } else {
+        respond.status(404).send('Contact not found')
+    }
+}) 
 
 
 module.exports = app
