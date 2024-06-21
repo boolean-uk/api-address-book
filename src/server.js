fba5bff8e934607d7e581a249049ecfe30a5b02a
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-06-21 16:48:50 +0100

    Core and pre-set up

--- src/server.js
+++ src/server.js
@@ -9,5 +9,20 @@ app.use(express.json())
 
 // write your app code here
 
+let contacts = require("../data/contacts.js")
+
+app.get("/contacts", (req, res)=>{
+    res.json({
+        contacts
+    })
+})
+
+app.post("/contacts", (req, res)=>{})
+
+app.get("/contacts/:id", (req, res)=>{})
+
+app.put("/contacts", (req, res)=>{})
+
+app.delete("/contacts", (req, res)=>{})
 
 module.exports = app
