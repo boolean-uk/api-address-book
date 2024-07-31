925a927906dcb69120ba9016aafe85cb71d72991
Thomas Jensen <thomas.jensen_@outlook.com>
2024-07-31 15:10:11 +0200

    Completed core (except DELETE)

--- src/server.js
+++ src/server.js
@@ -1,13 +1,63 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const contacts = require('../data/contacts')
+
 const app = express()
 
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 
-// write your app code here
+app.get('/contacts', (req, res) => {
+    res.status(200).json({ contacts: contacts })
+})
 
+app.get('/contacts/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const contact = contacts.find((contact) => {
+        return contact.id === id
+    })
+
+    res.status(200).json({ contact: contact })
+})
+
+let nextContactId = 3
+
+app.post('/contacts', (req, res) => {
+    const contact = req.body
+    contact.id = nextContactId
+
+    nextContactId++
+
+    contacts.push(contact)
+    res.status(201).json({ contact })
+})
+
+app.put('/contacts/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const updatedContact = req.body
+
+    const index = contacts.findIndex((contact) => {
+        return contact.id === id
+    })
+
+    contacts[index] = {
+        ...updatedContact,
+        id: id,
+    }
+
+    res.status(200).json({ contact: contacts[index] })
+})
+
+app.delete('/contacts/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const deletedContact = contacts.find((contact) => contact.id === id)
+    contacts = contacts.filter((contact) => contact.id !== id)
+
+    res.status(200).json({ contact: deletedContact })
+})
 
 module.exports = app
